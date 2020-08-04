36f193ff8942777d7c048fa04bdba27e872d8ba6
Ankcorn <thomasankcorn@gmail.com>
2020-08-04 09:15:03 +0100

    make some progress on tailwind and a cloudfront distrobution

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/main.css'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
