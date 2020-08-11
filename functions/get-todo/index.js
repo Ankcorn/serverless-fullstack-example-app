26d678a5bdc06f63028787d7d930c767d3a919ec
Ankcorn <thomasankcorn@gmail.com>
2020-08-11 23:57:20 +0100

    attempt 2 to fix api

--- functions/get-todo/index.js
+++ functions/get-todo/index.js
@@ -1,6 +1,5 @@
 import { Todo } from '../../data/dynamodb'
 
-
 export async function handler(e) {
 	console.log(e.requestContext.authorizer.claims);
 	const todo = await Todo.query(e.requestContext.authorizer.claims.sub);
