f6efa7f3577b7b9ed408bf3b6a1d8257be28695c
Ankcorn <thomasankcorn@gmail.com>
2020-08-12 22:46:12 +0100

    make lambdas support http 2.0 format

--- functions/add-todo/index.js
+++ functions/add-todo/index.js
@@ -1,9 +1,8 @@
 import { Todo } from '../../data/dynamodb'
 
 export async function handler(e) {
-	console.log(e.requestContext.authorizer.claims);
-	const todo = await Todo.put({
-		id: e.requestContext.authorizer.claims.sub,
+	await Todo.put({
+		id: e.requestContext.authorizer.jwt.claims.sub,
 		status: 'todo',
 		date_added: Date.now(),
 		message: JSON.parse(e.body).message
--- functions/get-todo/index.js
+++ functions/get-todo/index.js
@@ -1,8 +1,7 @@
 import { Todo } from '../../data/dynamodb'
 
 export async function handler(e) {
-	console.log(e.requestContext.authorizer.claims);
-	const todo = await Todo.query(e.requestContext.authorizer.claims.sub);
+	const todo = await Todo.query(e.requestContext.authorizer.jwt.claims.sub);
 	return {
 		statusCode: 200,
 		body: JSON.stringify(todo)
