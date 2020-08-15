f53b6d8d9d12f7bddbce2216849f781ee7bf9e6e
Ankcorn <thomasankcorn@gmail.com>
2020-08-15 22:59:08 +0100

    remove broken br

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,7 +8,6 @@ function Home() {
 					<div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
 						<h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
 							Helping you build software products
-        					<br class="hidden lg:inline-block" />
 							with serverless
       					</h1>
 						<p class="mb-8 leading-relaxed">
