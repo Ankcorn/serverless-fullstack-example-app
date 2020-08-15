395a2f6c44490f7517979b292af7e22882d38e61
Ankcorn <thomasankcorn@gmail.com>
2020-08-15 22:55:37 +0100

    add home copy

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,10 +6,15 @@ function Home() {
 			<section class="text-gray-500 bg-gray-900 body-font">
 				<div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
 					<div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-						<h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Before they sold out
-        <br class="hidden lg:inline-block" />readymade gluten
-      </h1>
-						<p class="mb-8 leading-relaxed">opper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+						<h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+							Helping you build software products
+        					<br class="hidden lg:inline-block" />
+							with serverless
+      					</h1>
+						<p class="mb-8 leading-relaxed">
+							Serverless promises that it helps you focus on your core product but because the tooling is complicated it can often be hard to achieve that.
+							Serverless Sass Starter is a high quality opinionated boilerplate that supercharges your serverless development process.
+						</p>
 						<div class="flex justify-center">
 							<button class="inline-flex text-white bg-teal-700 border-0 py-2 px-6 focus:outline-none hover:bg-teal-700 rounded text-lg">Button</button>
 							<button class="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">Button</button>
