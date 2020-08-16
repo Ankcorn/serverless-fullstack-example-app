f8d4342a16bc9665e5630a250a74de377c2f7336
Ankcorn <thomasankcorn@gmail.com>
2020-08-16 17:20:41 +0100

    implement new components to clean up homepage code

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,110 +1,66 @@
 import React from 'react';
+import Hero from '../components/hero';
+import Tools from '../components/tools';
+import AWS from '../assets/logos/aws';
+import Serverless from '../assets/logos/serverless';
+import Node from '../assets/logos/node';
+import ReactLogo from '../assets/logos/react';
+import Tailwind from '../assets/logos/tailwind'
 
 function Home() {
 	return (
 		<div className="App">
-			<section class="text-gray-500 bg-gray-900 body-font">
-				<div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-					<div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-						<h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-							Helping you build software products
-							with serverless
-      					</h1>
-						<p class="mb-8 leading-relaxed">
-							Serverless promises that it helps you focus on your core product but because the tooling is complicated it can often be hard to achieve that.
-							Serverless Sass Starter is a high quality opinionated boilerplate that supercharges your serverless development process.
-						</p>
-						<div class="flex justify-center">
-							<button class="inline-flex text-white bg-teal-700 border-0 py-2 px-6 focus:outline-none hover:bg-teal-700 rounded text-lg">Button</button>
-							<button class="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">Button</button>
-						</div>
-					</div>
-					<div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-						<img class="object-cover object-center rounded" alt="hero" src="https://images.unsplash.com/photo-1498084393753-b411b2d26b34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&w=720&h=600&q=80" />
-					</div>
-				</div>
-			</section>
-			<section class="text-gray-500 body-font bg-gray-900">
-				<div class="container px-5 py-24 mx-auto">
-					<div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
-						<h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">Pitchfork Kickstarter Taxidermy</h1>
-						<p class="lg:w-1/2 w-full leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table.</p>
-					</div>
-					<div class="flex flex-wrap -m-4">
-						<div class="xl:w-1/3 md:w-1/2 p-4">
-							<div class="border border-gray-800 p-6 rounded-lg">
-								<div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-gray-800 text-teal-400 mb-4">
-									<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
-										<path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-									</svg>
-								</div>
-								<h2 class="text-lg text-white font-medium title-font mb-2">Shooting Stars</h2>
-								<p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
-							</div>
-						</div>
-						<div class="xl:w-1/3 md:w-1/2 p-4">
-							<div class="border border-gray-800 p-6 rounded-lg">
-								<div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-gray-800 text-teal-400 mb-4">
-									<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
-										<circle cx="6" cy="6" r="3"></circle>
-										<circle cx="6" cy="18" r="3"></circle>
-										<path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
-									</svg>
-								</div>
-								<h2 class="text-lg text-white font-medium title-font mb-2">The Catalyzer</h2>
-								<p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
-							</div>
-						</div>
-						<div class="xl:w-1/3 md:w-1/2 p-4">
-							<div class="border border-gray-800 p-6 rounded-lg">
-								<div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-gray-800 text-teal-400 mb-4">
-									<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
-										<path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-										<circle cx="12" cy="7" r="4"></circle>
-									</svg>
-								</div>
-								<h2 class="text-lg text-white font-medium title-font mb-2">Neptune</h2>
-								<p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
-							</div>
-						</div>
-						<div class="xl:w-1/3 md:w-1/2 p-4">
-							<div class="border border-gray-800 p-6 rounded-lg">
-								<div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-gray-800 text-teal-400 mb-4">
-									<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
-										<path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1zM4 22v-7"></path>
-									</svg>
-								</div>
-								<h2 class="text-lg text-white font-medium title-font mb-2">Melanchole</h2>
-								<p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
-							</div>
-						</div>
-						<div class="xl:w-1/3 md:w-1/2 p-4">
-							<div class="border border-gray-800 p-6 rounded-lg">
-								<div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-gray-800 text-teal-400 mb-4">
-									<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
-										<path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"></path>
-									</svg>
-								</div>
-								<h2 class="text-lg text-white font-medium title-font mb-2">Bunker</h2>
-								<p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
-							</div>
-						</div>
-						<div class="xl:w-1/3 md:w-1/2 p-4">
-							<div class="border border-gray-800 p-6 rounded-lg">
-								<div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-gray-800 text-teal-400 mb-4">
-									<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
-										<path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
-									</svg>
-								</div>
-								<h2 class="text-lg text-white font-medium title-font mb-2">Ramona Falls</h2>
-								<p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
-							</div>
-						</div>
-					</div>
-					<button class="flex mx-auto mt-16 text-white bg-teal-500 border-0 py-2 px-8 focus:outline-none hover:bg-teal-600 rounded text-lg">Button</button>
-				</div>
-			</section>
-			<section class="text-gray-500 bg-gray-900 body-font">
+			<Hero
+				title="Helping you build software products with serverless"
+				description={`Serverless promises that it helps you focus on your core product
+				but because the tooling is complicated it can often be hard to achieve that.
+				Serverless Sass Starter is a high quality opinionated boilerplate that supercharges
+				your serverless development process.`}
+				image="https://images.unsplash.com/photo-1597509288493-c1fff98a4bd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&crop=center&w=720&h=600&q=80"
+			/>
+			<Tools
+				title="Components"
+				description="Serverless sass starter uses"
+				tools={[
+					{
+						name: "Amazon Web Services",
+						description: "Serverless web hosting, compute, and storage.",
+						link: "https://aws.amazon.com/",
+						icon: AWS
+					},
+					{
+						name: "Serverless Framework",
+						description: "Zero-friction serverless infrastructure deployment as code",
+						link: "https://www.serverless.com/",
+						icon: Serverless
+					},
+					{
+						name: "Node.js",
+						description: "Zero-friction serverless infrastructure deployment as code",
+						link: "https://www.serverless.com/",
+						icon: Node
+					},
+					{
+						name: "React",
+						description: "Serverless web hosting, compute, and storage.",
+						link: "https://aws.amazon.com/",
+						icon: ReactLogo
+					},
+					{
+						name: "Serverless Framework",
+						description: "Zero-friction serverless infrastructure deployment as code",
+						link: "https://www.serverless.com/",
+						icon: Tailwind
+					},
+					{
+						name: "Node.js",
+						description: "Zero-friction serverless infrastructure deployment as code",
+						link: "https://www.serverless.com/",
+						icon: Node
+					},
+				]}
+			/>
+			< section class="text-gray-500 bg-gray-900 body-font" >
 				<div class="container px-5 py-24 mx-auto">
 					<div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
 						<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8 text-gray-700 mb-8" viewBox="0 0 975.036 975.036">
@@ -116,10 +72,10 @@ function Home() {
 						<p class="text-gray-400">Senior Product Designer</p>
 					</div>
 				</div>
-			</section>
-			<footer class="text-gray-500 bg-gray-900 body-font">
+			</section >
+			<footer class="text-gray-700 bg-white body-font">
 				<div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-					<a class="flex title-font font-medium items-center md:justify-start justify-center text-white">
+					<a class="flex title-font font-medium items-center md:justify-start justify-center">
 						<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-teal-500 rounded-full" viewBox="0 0 24 24">
 							<path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
 						</svg>
@@ -154,7 +110,7 @@ function Home() {
 					</span>
 				</div>
 			</footer>
-		</div>
+		</div >
 	);
 }
 
