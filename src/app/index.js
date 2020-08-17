75643cd99ba98aa1ab3a767b924dd76c365f03df
Ankcorn <thomasankcorn@gmail.com>
2020-08-17 22:37:14 +0100

    make form input controlled and remove text on submit

--- src/app/index.js
+++ src/app/index.js
@@ -35,6 +35,7 @@ function App() {
 		e.preventDefault();
 		await create({ message: newTodo })
 		await revalidate()
+		setNewTodo('');
 	}
 
 	let today = [];
@@ -59,7 +60,7 @@ function App() {
 			<section className="container mx-auto px-5 pt-16 space-y-8">
 				<div className="flex flex-col justify-center items-center space-y-8">
 					<form onSubmit={handleSubmit} class="flex w-full justify-center">
-						<input onChange={(e) => setNewTodo(e.target.value)} class="bg-gray-100 rounded mr-4 border border-gray-400 focus:outline-none focus:border-teal-700 text-base px-4 lg:w-full xl:w-1/2 w-2/4 md:w-full" placeholder="Placeholder" type="text" />
+						<input value={newTodo} onChange={(e) => setNewTodo(e.target.value)} class="bg-gray-100 rounded mr-4 border border-gray-400 focus:outline-none focus:border-teal-700 text-base px-4 lg:w-full xl:w-1/2 w-2/4 md:w-full" placeholder="Placeholder" type="text" />
 						<button type="submit" class="inline-flex text-white bg-teal-700 border-0 py-2 px-6 focus:outline-none hover:bg-teal-900 rounded text-lg">Create</button>
 					</form>
 				</div>
