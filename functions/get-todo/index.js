b866016caa5503bd2871cb036ba04bb4fa11b3a7
Ankcorn <thomasankcorn@gmail.com>
2020-08-26 11:58:26 +0100

    some logging and playing

--- functions/get-todo/index.js
+++ functions/get-todo/index.js
@@ -1,6 +1,9 @@
 import { Todo } from '../../data/dynamodb'
+import tracer from 'tracer';
 
+const logger = tracer.colorConsole()
 export async function handler(e) {
+	logger.info('Request for todo list', e.requestContext.authorizer.jwt.claims)
 	const todo = await Todo.query(e.requestContext.authorizer.jwt.claims.sub);
 	return {
 		statusCode: 200,
