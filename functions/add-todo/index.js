0cc025a5a075c83175876f16ff953b5eae6df6bf
Ankcorn <thomasankcorn@gmail.com>
2020-08-29 19:52:45 +0100

    try invalidate cache for updates

--- functions/add-todo/index.js
+++ functions/add-todo/index.js
@@ -1,10 +1,23 @@
 import { Todo } from '../../data/dynamodb'
 import tracer from 'tracer';
-
+import AWS from 'aws-sdk';
 const logger = tracer.colorConsole()
+const cloudFront = new AWS.CloudFront();
 export async function handler(e) {
 	
 	try {
+		await cloudFront.createInvalidation({
+			DistributionId: process.env.DISTRIBUTION_ID,
+			InvalidationBatch: {
+				Paths: {
+					Quantity: 1,
+					Items: [
+						e.requestContext.authorizer.claims.sub
+					]
+				}
+			}
+		}).promise();
+
 		await Todo.put({
 			id: e.requestContext.authorizer.claims.sub,
 			status: 'todo',
